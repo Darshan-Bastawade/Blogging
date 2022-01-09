bbd28a6356bbc86e169fc297337cb8ba729c6f82
MrDB07 <74754704+MrDB07@users.noreply.github.com>
2022-01-09 07:43:58 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+  <div class="footer"> 
+    
+    <p style=" float: left; text-align: center;">© Copyright 2022 Blogging | Powered by MR DB</p>
+    
+  </div>
+
+</footer>
---
+++ index.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+
+
+
+<body>
+  <?php
+  include ("header.php");
+  ?>
+  <!--1 st row-->
+  <div class="card" style="width: 18rem; margin: 20px;  float: left; margin-left: 20px; ">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+        content.</p>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem; margin: 20px;  float: left;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+        content.</p>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem; margin: 20px;  float: left;">
+    <img src="Images/Coder.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+        content.</p>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem; margin: 20px; float: left;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+        content.</p>
+    </div>
+  </div>
+<!--2 nd row-->
+
+
+<div class="card" style="width: 18rem; margin: 20px;  float: left; margin-left: 20px; clear: both;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+      content.</p>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem; margin: 20px;  float: left; margin-left: 20px; ">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+      content.</p>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem; margin: 20px;  float: left; margin-left: 20px; ">
+  <img src="Images/logo.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+      content.</p>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem; margin: 20px;  float: left; margin-left: 20px; ">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+      content.</p>
+  </div>
+</div>
+
+<?php
+include ("footer.php");
+?>
+</body>
+
+</html>
